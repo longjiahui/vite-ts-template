91e92dbb1c35fb93f8619e3227086d0bd38dc6d5
Anfo <longjiahui@hotmail.com>
2023-05-30 10:05:25 +0800

    feat: lib mode refactor

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import App from './App.vue'
+import { isDev } from './const'
 import './style.scss'
+import DefaultLib from '@/index'
 import plugins from '@/scripts/plugins'
 import { createApp } from 'vue'
 
@@ -7,4 +9,15 @@ const app = createApp(App)
 
 app.use(plugins)
 
-app.mount('#app')
+const module = isDev
+    ? import.meta.glob('@/index')
+    : import.meta.glob('../dist/huitr.js')
+
+;(async () => {
+    const [{ default: defaultLib }] = (await Promise.all(
+        Object.keys(module).map((k: keyof typeof module) => module[k]()),
+    )) as [{ default: typeof DefaultLib }]
+    defaultLib()
+})().finally(() => {
+    app.mount('#app')
+})
--- vite.prod.config.ts
+++ vite.prod.config.ts
@@ -2,12 +2,36 @@ import baseConfig from './vite.base.config'
 import vue from '@vitejs/plugin-vue'
 import { defineConfig, mergeConfig } from 'vite'
 import viteCompression from 'vite-plugin-compression'
+import dts from 'vite-plugin-dts'
 
 // https://vitejs.dev/config/
 export default mergeConfig(
     baseConfig,
     defineConfig({
-        plugins: [vue(), viteCompression()],
+        plugins: [
+            vue(),
+            viteCompression(),
+            dts({
+                include: 'src/index.ts',
+            }),
+        ],
         base: './',
+        build: {
+            lib: {
+                formats: ['es', 'umd', 'iife'],
+                entry: 'src/index.ts',
+                name: 'huitr',
+                fileName: 'huitr',
+            },
+            rollupOptions: {
+                external: ['vue', 'vue-router'],
+                output: {
+                    globals: {
+                        vue: 'Vue',
+                        'vue-router': 'VueRouter',
+                    },
+                },
+            },
+        },
     }),
 )
