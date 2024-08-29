3f48a1d9a919ec87f415fad177cf302f110a358b
Anfo <longjiahui@hotmail.com>
2024-08-29 16:56:00 +0800

    feat: 升级依赖版本，去掉eslint，husky升级

--- vite.dev.base.config.ts
+++ vite.dev.base.config.ts
@@ -1,12 +1,11 @@
 import baseConfig from './vite.base.config'
 import vue from '@vitejs/plugin-vue'
 import { defineConfig, mergeConfig } from 'vite'
-import eslint from 'vite-plugin-eslint'
 
 // https://vitejs.dev/config/
 export default mergeConfig(
     baseConfig,
     defineConfig({
-        plugins: [vue(), eslint()],
+        plugins: [vue()],
     }),
 )
